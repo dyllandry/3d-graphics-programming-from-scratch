d8644359ba0abbb2ac256af5d4f3c52090f95509
dylan <dylanglandry@gmail.com>
2024-03-09 15:28:08 -0500

    render color buffer on screen

--- 3drenderer/src/main.c
+++ 3drenderer/src/main.c
@@ -4,10 +4,12 @@
 #include <SDL2/SDL.h>
 
 bool is_running = false;
+
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
 
 uint32_t* color_buffer = NULL;
+SDL_Texture* color_buffer_texture = NULL;
 
 int window_width = 800;
 int window_height = 600;
@@ -50,6 +52,14 @@ bool initialize_window(void) {
 void setup(void) {
     int num_pixels = window_width * window_height;
     color_buffer = malloc(sizeof *color_buffer * num_pixels);
+
+    color_buffer_texture = SDL_CreateTexture(
+        renderer,
+        SDL_PIXELFORMAT_ARGB8888,
+        SDL_TEXTUREACCESS_STREAMING,
+        window_width,
+        window_height
+    );
 }
 
 void process_input(void) {
@@ -73,9 +83,37 @@ void update(void) {
 
 }
 
+void render_color_buffer(void) {
+    SDL_UpdateTexture(
+        color_buffer_texture,
+        NULL,
+        color_buffer,
+        // row size
+        (int)(sizeof *color_buffer * window_width)
+    );
+    SDL_RenderCopy(
+        renderer,
+        color_buffer_texture,
+        NULL,
+        NULL
+    );
+}
+
+void clear_color_buffer(uint32_t color) {
+    for (int y = 0; y < window_height; y++) {
+        for (int x = 0; x < window_width; x++) {
+            color_buffer[(window_width * y) + x] = color;
+        }
+    }
+}
+
 void render(void) {
     SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
     SDL_RenderClear(renderer);
+
+    render_color_buffer();
+    clear_color_buffer(0xFFFFFF00);
+
     SDL_RenderPresent(renderer);
 }
 
