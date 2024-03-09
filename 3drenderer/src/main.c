1a51a333f71fa9b58963637b1defe86580bbf70e
dylan <dylanglandry@gmail.com>
2024-03-09 14:29:46 -0500

    add game loop

--- 3drenderer/src/main.c
+++ 3drenderer/src/main.c
@@ -1,6 +1,87 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
+
+bool is_running = false;
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+
+// Returns bool if SDL initialization worked.
+bool initialize_window(void) {
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        fprintf(stderr, "Error initializing SDL.\n");
+        return false;
+    }
+
+    window = SDL_CreateWindow(
+        NULL,                   // window title
+        SDL_WINDOWPOS_CENTERED, // window pos x
+        SDL_WINDOWPOS_CENTERED, // window pos y
+        800,                    // window width
+        600,                    // window height
+        SDL_WINDOW_BORDERLESS
+    );
+
+    if (!window) {
+        fprintf(stderr, "Errror creating SDL window.\n");
+        return false;
+    }
+
+    renderer = SDL_CreateRenderer(
+        window,
+        -1, // display device, -1 is default.
+        0   // extra flags
+    );
+
+    if (!renderer) {
+        fprintf(stderr, "Error creating SDL renderer.\n");
+        return false;
+    }
+
+    return true;
+}
+
+void setup(void) {
+
+}
+
+void process_input(void) {
+    SDL_Event event;
+    SDL_PollEvent(&event);
+
+    switch (event.type) {
+        // When the window's X button is pressed.
+        case SDL_QUIT:
+            is_running = false;
+            break;
+        case SDL_KEYDOWN:
+            if (event.key.keysym.sym == SDLK_ESCAPE) {
+                is_running = false;
+            }
+            break;
+    }
+}
+
+void update(void) {
+
+}
+
+void render(void) {
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+}
 
 int main(void) {
-    printf("Hello, world!\n");
+    is_running = initialize_window();
+
+    setup();
+
+    while (is_running) {
+        process_input();
+        update();
+        render();
+    }
+
     return 0;
 }
